1a7f3659484ab128214ee3d66f8922ca5f0d8125
Alesia-Avramenko <Avramenko.alesia@yandex.ru>
2024-05-28 19:05:51 +0300

    Исправлены ошибки в Lesson6.

--- src/Employee.java
+++ src/Employee.java
@@ -1,13 +1,13 @@
 
 public class Employee {
 
-    public int id;
-    public String name;
-    public String position;
-    public String email;
-    public String phoneNumber;
-    public int salary;
-    public int age;
+    private int id;
+    private String name;
+    private String position;
+    private String email;
+    private String phoneNumber;
+    private int salary;
+    private int age;
 
     public Employee(int id, String name, String position, String email, String phoneNumber, int salary, int age) {
         this.id = id;
@@ -23,5 +23,3 @@ public class Employee {
         System.out.println("Id: " + id + "; Имя пользователя: " + name + "; должность: " + position + "; email: " + email + "; телефон: " + phoneNumber + "; зарплата: " + salary + "; возраст: " + age);
     }
 }
-
-
